9f0ad2e8ef64c91e13faef04ffb2efa1fe8afe27
AnnVynnychenkoa <avinnichenko93@gmail.com>
2026-06-13 02:45:31 +0200

    feat: add JS converter logic

--- src/projects/converter/app-converter.js
+++ src/projects/converter/app-converter.js
@@ -1,6 +1,34 @@
 import units from './units-data.json';
-import convertedData from './entry-data.json';
 
-const convertedValue = convertedData.distance.value;
+const converterForm = document.querySelector('.converter-form');
+const result = document.querySelector('#result');
 
-console.log(loadConversionRules());
+function isValidNumber(value) {
+  if (Number.isNaN(value) || value < 0) {
+    alert('Please enter a valid positive number');
+    return false;
+  }
+  return true;
+}
+
+function calculateConversion(convertedData, fromUnit, toUnit) {
+  const fromRate = units[fromUnit];
+  const toRate = units[toUnit];
+  return (convertedData * fromRate) / toRate;
+}
+
+converterForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const fromUnit = formData.get('fromUnit');
+  const toUnit = formData.get('toUnit');
+  const convertedData = Number.parseFloat(formData.get('convertedData'));
+
+  if (!isValidNumber(convertedData)) {
+    return;
+  }
+  const finalResult = calculateConversion(convertedData, fromUnit, toUnit);
+
+  result.textContent = `${finalResult.toFixed(2)} ${toUnit}`;
+});

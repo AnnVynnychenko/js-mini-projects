638778fda0da294b840a129cf32e96488d4d6811
AnnVynnychenkoa <avinnichenko93@gmail.com>
2026-06-14 16:37:07 +0200

    feat: add CSS styles for Distance Converter

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        converter: resolve(__dirname, 'src/projects/converter/index.html'),
+        converter: resolve(__dirname, 'src/projects/converter/converter.html'),
       },
     },
   },
